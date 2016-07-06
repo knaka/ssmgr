ad05e47a3be224efe167be9ac658af64a0361e96
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2016-07-06 14:37:15 +0900

    No comment

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject._
-import play.api._
+// import play.api._
 import play.api.mvc._
 
 /**
@@ -19,6 +19,6 @@ class HomeController @Inject() extends Controller {
    */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+    // views.html.index("Hello Message")
   }
-
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -16,3 +16,7 @@ addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-mocha" % "1.1.0")
 
 addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.2")
+
+// addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "latest.integration")
+addSbtPlugin("org.ensime" % "sbt-ensime" % "0.6.0")
+
